902f1971a39c1559a41c954eaa29539b6b428137
Anderson Guedes <anderson.guedes@outlook.com.br>
2021-01-11 15:55:26 -0300

    Estruturação básica do header, main e footer

--- src/App.js
+++ src/App.js
@@ -3,21 +3,16 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="grid-container">
+      <header>
+        <a href="/">T-SHIRT STORE</a>
       </header>
+      <main>
+        Lista de Produtos
+      </main>
+      <footer>
+        2021 Anderson Guedes | T-Shirt Store
+      </footer>
     </div>
   );
 }
